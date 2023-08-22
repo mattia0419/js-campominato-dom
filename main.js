069ada246dcdf5e6db95499e1e52a255dd86b703
mattia0419 <pironemattia6@gmail.com>
2023-08-22 17:01:11 +0200

    add game over

---
+++ main.js
@@ -0,0 +1,74 @@
+const buttonGenerator = document.getElementById("generator");
+
+const difficultyselect = document.getElementById("difficult");
+let score = 0;
+let bombe = [];
+const grid = document.getElementById("griglia");
+
+buttonGenerator.addEventListener("click", function(){
+     
+    const size = parseInt(difficultyselect.value);
+    
+    bombe = wrongNumbers(1, size * size);
+    console.log(bombe);
+    generaGriglia(size);
+    
+})
+
+
+
+
+function wrongNumbers(min, max){
+    const randomNum = [];
+    while(randomNum.length < 16){
+        const numCasual = Math.floor(Math.random() * (max - min + 1)) + min;
+        if(!randomNum.includes(numCasual)){
+            randomNum.push(numCasual);
+        }    
+    }
+    return randomNum;
+}
+
+
+function generaGriglia(size){
+    
+    
+    grid.innerHTML = "";
+
+
+    for(let i = 1; i <= size * size; i++){
+        
+        const cell = document.createElement("div");
+        
+        if(size == 10){
+            cell.classList.add("cell");
+        }
+        else if(size == 9){
+            cell.classList.add("cell-9");
+        }
+        else 
+            cell.classList.add("cell-7");
+        
+        cell.innerText = i;
+
+
+        cell.addEventListener("click", function(){
+            if(bombe.includes(i)){
+                this.classList.add("red");
+                grid.innerHTML = "";
+                
+                alert("HAI PERSO : IL TUO PUNTEGGIO E' " + score);
+                score = 0;
+            }
+            else
+            score++; 
+            this.classList.add("green");
+            
+            console.log(score);
+
+            
+        })
+        grid.append(cell);
+    }
+    
+}
